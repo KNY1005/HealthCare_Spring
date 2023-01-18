f6da95df89ce480f5bb43e7165f5e2d998fe476a
kseongju <kim2000730@gmail.com>
2023-01-18 16:46:26 +0900

    questionDAO 수정

--- src/main/java/edu/study/dao/QuestionDAO.java
+++ src/main/java/edu/study/dao/QuestionDAO.java
@@ -16,11 +16,11 @@ public class QuestionDAO {
 	private SqlSession sqlSession;
 
 	public BoardVo selectByBidx(int bidx) {	
-		return sqlSession.selectOne("edu.study.mapper.boardMapper.selectByBidx", bidx); //筌띾뜆�뵠獄쏅��뼒占쎈뮞揶쏉옙 占쎌젫�⑨옙,占쎈립椰꾬옙 鈺곌퀬�돳
+		return sqlSession.selectOne("edu.study.mapper.boardMapper.selectByBidx", bidx); 
 	}
 
 	public List<BoardVo> listSearch(SearchCriteria scri) {
-		
+		scri.setBlist("Q");
 		return sqlSession.selectList("edu.study.mapper.boardMapper.listSearch",scri);
 	}	
 	public int listCount(SearchCriteria scri) {
@@ -28,8 +28,8 @@ public class QuestionDAO {
 	}
 	
 	
-	public int updateByBidx(BoardVo vo) {	//int 筌뤿돂援뷂옙�뵠 筌ｌ꼶�봺占쎈┷占쎈�占쎈뮉揶쏉옙(燁삳똻�뒲占쎈뱜 揶쏉옙)
-		return sqlSession.update("edu.study.mapper.boardMapper.updateByBidx", vo); //占쎈씜占쎈쑓占쎌뵠占쎈뱜占쎈뮉 野껉퀗�궢�빊�뮆�젾 占쎈씨占쎌벉
+	public int updateByBidx(BoardVo vo) {	
+		return sqlSession.update("edu.study.mapper.boardMapper.updateByBidx", vo);
 	}
 	
 	public int insert(BoardVo vo) {	
