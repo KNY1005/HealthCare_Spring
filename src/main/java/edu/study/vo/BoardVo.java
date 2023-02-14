0c09793f78716b56395823d0310c93c37b7f329c
kseongju <kim2000730@gmail.com>
2023-02-14 09:24:16 +0900

    ㅅㄷㄴㅅ

--- src/main/java/edu/study/vo/BoardVo.java
+++ src/main/java/edu/study/vo/BoardVo.java
@@ -17,7 +17,6 @@ public class BoardVo {
 	private String bdelete;
 	private String bsecret;
 	private Integer bhit;
-	private String mname;
 	private int midx;
 	
 	
@@ -133,12 +132,6 @@ public class BoardVo {
 				+ ", blist=" + blist + ", bcategory=" + bcategory + ", btitle=" + btitle + ", bcontent=" + bcontent
 				+ ", bwdate=" + bwdate + ", brewdate=" + brewdate + ", bstate=" + bstate + ", bmdate=" + bmdate
 				+ ", bdelete=" + bdelete + ", bsecret=" + bsecret + ", midx=" + midx + "]";
-	}
-	public String getMname() {
-		return mname;
-	}
-	public void setMname(String mname) {
-		this.mname = mname;
 	}
 	
 	
